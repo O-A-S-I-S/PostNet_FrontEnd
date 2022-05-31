8b2d7beafc0a8dcb48789c51a97ba584633c4b2e
Roberto Basauri <89091501+RobertoCarlosBasauriQuispe@users.noreply.github.com>
2022-05-31 18:23:00 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { InitComponent } from './components/init/init.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppointmentsComponent } from './components/appointments/appointments.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { PrescriptionsComponent } from './components/prescriptions/prescriptions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppointmentsComponent,
+    InitComponent,
+    PrescriptionsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgxPaginationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
