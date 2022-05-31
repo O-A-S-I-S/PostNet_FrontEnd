5106d75999340c3780ef220535c433c89d1c46c7
Roberto Basauri <89091501+RobertoCarlosBasauriQuispe@users.noreply.github.com>
2022-05-31 18:25:01 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { PrescriptionsComponent } from './components/prescriptions/prescriptions.component';
+import { InitComponent } from './components/init/init.component';
+import { AppointmentsComponent } from './components/appointments/appointments.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'landingpage',pathMatch: 'full'},
+  {path:'landingpage', component: InitComponent},
+  {path:'appointments/:id', component: AppointmentsComponent},
+  {path:'prescriptions/:patientid/:appointmentid', component: PrescriptionsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
